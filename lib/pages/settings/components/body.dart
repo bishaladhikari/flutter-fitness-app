e5b0f9cdc1610095a4a61fd8b41a7ce0d641c941
Prabin Paudel <prabin.paudel60@gmail.com>
2020-09-05 17:42:56 +0545

    update

--- lib/pages/settings/components/body.dart
+++ lib/pages/settings/components/body.dart
@@ -2,7 +2,12 @@ import 'package:ecapp/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:ecapp/components/search_box.dart';
 
-class Body extends StatelessWidget {
+class Body extends StatefulWidget {
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -11,12 +16,92 @@ class Body extends StatelessWidget {
         children: <Widget>[
           ListTile(title: Text('Account Information')),
           ListTile(title: Text('Address Book')),
-          ListTile(title: Text('Language')),
+          ListTile(
+              title: Text('Language'),
+              onTap: () {
+                _languageChange(context);
+              }),
           ListTile(title: Text('Policies')),
           ListTile(title: Text('About')),
           ListTile(title: Text('Help')),
         ],
       ),
     );
+  }
+
+  void _languageChange(context) {
+
+
+    String radioItem = "";
+
+    showModalBottomSheet(
+        context: context,
+        builder: (BuildContext bc) {
+          return Container(
+            height: MediaQuery.of(context).size.height * .50,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Text("Language Information"),
+                      Spacer(),
+                      IconButton(
+                        icon:
+                            Icon(Icons.cancel, color: Colors.orange, size: 25),
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                      )
+                    ],
+                  ),
+                  Row(children: <Widget>[
+                    RadioListTile(
+                      groupValue: radioItem,
+                      title: Text('Nepali'),
+                      value: 'Nepali',
+                      onChanged: (val) {
+                        setState(() {
+                          radioItem = val;
+                        });
+                      },
+                    ),
+                    // RadioListTile(
+                    //   groupValue: radioItem,
+                    //   title: Text('Japanese'),
+                    //   value: 'Item 1',
+                    //   onChanged: (val) {},
+                    // ),
+                    // RadioListTile(
+                    //   groupValue: radioItem,
+                    //   title: Text('English'),
+                    //   value: 'Item 1',
+                    //   onChanged: (val) {},
+                    // )
+                  ]),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      RaisedButton(
+                        onPressed: () {},
+                        padding: const EdgeInsets.symmetric(horizontal: 96.0),
+                        color: Color(0xFFDA1D21),
+                        textColor: Colors.white,
+                        child: Text("SUBMIT"),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(30),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+          );
+        });
   }
 }
