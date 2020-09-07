9d47ea9bcccbf41a498a79002e54ff63429bc8f6
krisee <krishiyana19@gmail.com>
2020-09-07 22:14:17 +0545

    name , address, area, shiping,  biling address

--- lib/pages/details/components/add-address.dart
+++ lib/pages/details/components/add-address.dart
@@ -1,33 +1,31 @@
 import 'package:flutter/material.dart';
+// import 'package:ecapp/pages/details/components/add-address.dart';
 
 void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: AddressPage(),
     ));
 
-class HomePage extends StatelessWidget {
+class AddressPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: new AppBar(
           backgroundColor: Colors.white,
           centerTitle: true,
+          leading: Text(
+            'Cancel',
+            textAlign: TextAlign.right,
+            style: new TextStyle(
+              fontSize: 16.0,
+              color: Colors.black,
+            ),
+          ),
           title: new Text(
             'Add New Address',
             style: TextStyle(color: Colors.black, fontSize: 18),
           ),
-          actions: <Widget>[
-            new Center(
-              child: Text(
-                'Cancel',
-                textAlign: TextAlign.right,
-                style: new TextStyle(
-                  fontSize: 16.0,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-          ],
+          
         ),
         body: Container(
             padding:
@@ -36,94 +34,110 @@ class HomePage extends StatelessWidget {
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
                   TextField(
-                    decoration: InputDecoration(
+                      decoration: InputDecoration(
                     hintText: "Full Name",
-                     hintStyle: TextStyle(color:Colors.grey),
-                    )
-                  ),
+                    hintStyle: TextStyle(color: Colors.grey),
+                  )),
                   TextField(
-                     decoration: InputDecoration(
+                      decoration: InputDecoration(
                     hintText: "Phone Number",
-                     hintStyle: TextStyle(color:Colors.grey),
-                    )
-
-                  ),
+                    hintStyle: TextStyle(color: Colors.grey),
+                  )),
                   TextField(
-                     decoration: InputDecoration(
+                      decoration: InputDecoration(
                     hintText: "Region",
-                     hintStyle: TextStyle(color:Colors.grey),
-                    )
-                  ),
+                    hintStyle: TextStyle(color: Colors.grey),
+                  )),
                   TextField(
-                     decoration: InputDecoration(
+                      decoration: InputDecoration(
                     hintText: "City",
-                     hintStyle: TextStyle(color:Colors.grey),
-                    )
-                  ),
+                    hintStyle: TextStyle(color: Colors.grey),
+                  )),
                   TextField(
-                     decoration: InputDecoration(
+                      decoration: InputDecoration(
                     hintText: "Area",
-                     hintStyle: TextStyle(color:Colors.grey),
-                    )
-                  ),
+                    hintStyle: TextStyle(color: Colors.grey),
+                  )),
                   TextField(
-                     decoration: InputDecoration(
+                      decoration: InputDecoration(
                     hintText: "Address",
-                     hintStyle: TextStyle(color:Colors.grey),
-                    )
-                  ),
-
-                    
+                    hintStyle: TextStyle(color: Colors.grey),
+                  )),
                   Container(
-                    padding:const EdgeInsets.fromLTRB(0,20,0,20),
-                    
-                    child: Text("Select a label for effective delivery",style: TextStyle(color: Colors.black,fontSize:17),),
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                    child: Text(
+                      "Select a label for effective delivery",
+                      style: TextStyle(color: Colors.black, fontSize: 17),
+                    ),
                   ),
-                            
-                          
-              Row(
-  children: <Widget>[
-    FlutterLogo(),
-    Expanded(child: SizedBox()),
-    FlutterLogo(),
-  ],
-),
-            
-                
+                  Row(
+                    children: <Widget>[
+                      Container(
+                         margin: new EdgeInsets.symmetric(horizontal: 50.0),
+                          height: 50,
+                          width: 80,
+                          decoration: BoxDecoration(
+                             border: Border.all(color: Colors.blue,
+                         width: 1.0,),
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white),
+                          child: Center(
+                              child: Row(children: <Widget>[
+                            FlutterLogo(),
+                            Text(
+                              'Home',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 18),
+                            )
+                          ]))),
+                      Expanded( child: Container(
+                        margin: new EdgeInsets.symmetric(horizontal: 50.0),
+                        width: 100,
+               height:50,
+               decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blue,
+                         width: 1.0,),
+                 borderRadius: BorderRadius.circular(10),
+                 color: Colors.white
+               ),
+               child: Center(
+                              child: Row(children: <Widget>[
+                            FlutterLogo(),
+                            Text(
+                              'Office',
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 18),
+                            )
+                          ]))),
+             ),]
+                      
+                      
                     
-                    
- 
-                  
-                   Container(
-                    padding:const EdgeInsets.fromLTRB(0,20,0,20),
-                    
-                    child: Text("Make a default shipping address",style: TextStyle(color: Colors.black,fontSize:17),),
                   ),
                   Container(
-                    padding:const EdgeInsets.fromLTRB(0,20,0,20),
-                    
-                    child: Text("Make a default biling address",style: TextStyle(color: Colors.black,fontSize:17),),
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                    child: Text(
+                      "Make a default shipping address",
+                      style: TextStyle(color: Colors.black, fontSize: 17),
+                    ),
                   ),
-
-
-                   Container(
-            height:35,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.orange[900]
-            ),
-              
-              child: Center(
-                
-                child: Text('Save',style:TextStyle(color: Colors.white, fontSize:18),)
-              ))
-
-                ]
-                )
-                
-                
-            )
-       );
-    
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                    child: Text(
+                      "Make a default biling address",
+                      style: TextStyle(color: Colors.black, fontSize: 17),
+                    ),
+                  ),
+                  Container(
+                      height: 35,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.orange[900]),
+                      child: Center(
+                          child: Text(
+                        'Save',
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      )))
+                ])));
   }
 }
--- lib/pages/settings/components/body.dart
+++ lib/pages/settings/components/body.dart
@@ -1,27 +1,29 @@
-import 'package:ecapp/constants.dart';
+// import 'package:ecapp/constants.dart';
+import 'package:ecapp/pages/details/components/add-address.dart';
 import 'package:flutter/material.dart';
-import 'package:ecapp/components/search_box.dart';
+// import 'package:ecapp/components/search_box.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[ 
-          
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
           ListTile(title: Text('Account Information')),
-          ListTile(title: Text('Address Book')),   
-          ListTile(title: Text('Language')),
-           
-          ListTile(title: Text('Policies')),
-          ListTile(title: Text('About')),
-          ListTile(title: Text('Help')),
-          
-        ],
-        
-      ),
-     
-    );
+          ListTile(
+              title: Text('Address Book'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => AddressPage()),
+                );
+                ListTile(title: Text('Language'));
+
+                ListTile(title: Text('Policies'));
+                ListTile(title: Text('About'));
+                ListTile(title: Text('Help'));
+              }),
+        ]));
   }
 }
