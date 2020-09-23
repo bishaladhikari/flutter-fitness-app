5cd720cb6064c6d95329b11488490e8a9630a7d5
Bishal <actionbishal98130@gmail.com>
2020-09-23 08:50:06 +0545

    updated cart-design

--- lib/pages/cart/components/CartBody.dart
+++ lib/pages/cart/components/CartBody.dart
@@ -10,7 +10,7 @@ class CartBody extends StatefulWidget {
 
 class _CartBodyState extends State<CartBody> {
   List cart = [
-    {'name': 'Cabbage', 'price': 'Rs 100', 'image': 'https://img2.pngio.com/cheese-pizza-png-clip-art-library-plain-pizza-png-920_805.png', 'count':1},
+    {'name': 'Cabbage goes the long way', 'price': 'Rs 100', 'image': 'https://img2.pngio.com/cheese-pizza-png-clip-art-library-plain-pizza-png-920_805.png', 'count':1},
     {'name': 'Tomato', 'price': 'Rs 50', 'image': 'https://pngimg.com/uploads/burger_sandwich/burger_sandwich_PNG4135.png', 'count': 5},
     {'name': 'Potato', 'price': 'Rs 250', 'image': 'https://lh3.googleusercontent.com/proxy/RmSFgLUs1MO_dQxup3bjAo9SCeo5kBup11gBI5Kqm-269CCqKfMouHQET1RULWXV1j6Nia5tzt4-dxzKoXI2CpG5e2N0W-zoRXG3vfKBJ6R-c5w6VAHCJDbssQ', 'count':2},
   ];
@@ -42,14 +42,13 @@ class _CartBodyState extends State<CartBody> {
       child: Column(
 //        mainAxisSize: Main,
         children: [
-          SizedBox(
-            height: 10,
-          ),
           ListView.builder(
             shrinkWrap: true,
+            padding: EdgeInsets.symmetric(vertical: 10),
             itemCount: cart.length,
             itemBuilder: (context, i) {
               return Container(
+                color: Colors.white,
                 padding: EdgeInsets.only(bottom: 1),
                 child: Row(
                   mainAxisSize: MainAxisSize.max,
@@ -72,9 +71,8 @@ class _CartBodyState extends State<CartBody> {
                     ),
                     Expanded(
                       child: Container(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 23),
-                        color: Colors.white,
+//                        padding:
+//                            EdgeInsets.symmetric(horizontal: 10, vertical: 23),
 //                        height: 100,
                         width: 200,
                         child: Column(
@@ -87,6 +85,7 @@ class _CartBodyState extends State<CartBody> {
                                     fontFamily: 'Quicksand',
                                     fontWeight: FontWeight.bold,
                                     color: Colors.black)),
+                            SizedBox(height: 10),
                             Text(cart[i]['price'], style: TextStyle(
                                 fontFamily: 'Quicksand',
                                 fontWeight: FontWeight.w400,
