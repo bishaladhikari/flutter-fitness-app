acba8fd4151e7ef8b3d94141f8002f1febc3fcc7
Bishal <actionbishal98130@gmail.com>
2020-10-03 23:36:16 +0545

    updated

--- lib/models/attribute.dart
+++ lib/models/attribute.dart
@@ -55,7 +55,7 @@ class Attribute {
     unit = json['unit'];
     actualQuantity = json['actual_quantity'];
     variant =
-         new Variant.fromJson(json['variant']) ?? null;
+        json['variant'] != "" ? new Variant.fromJson(json['variant']) : null;
     slug = json['slug'];
     saved = json['saved'];
   }
