f7ccea2201979996c5d3788c9582af076184e33f
Suu-Zan <koiralasuuzan16@gmail.com>
2020-10-05 10:58:42 +0545

    updated

--- lib/pages/address-book/add_location.dart
+++ lib/pages/address-book/add_location.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:dotted_border/dotted_border.dart';
+import '../../constants.dart';
 import '../details/components/../../address-book/add-address.dart';
 
 void main() => runApp(MaterialApp(
@@ -19,8 +20,10 @@ class LocationPage extends StatelessWidget {
             style: TextStyle(color: Colors.black, fontSize: 18),
           ),
         ),
-        body: ListView(children: <Widget>[
+        body: 
+        ListView(children: <Widget>[
           DottedBorder(
+            // padding: EdgeInsets.all(20.0),
             color: Colors.black,
             strokeWidth: 1,
             child: GestureDetector(
@@ -30,7 +33,7 @@ class LocationPage extends StatelessWidget {
               },
               child: Container(
                 // width: 310.0,
-                height: 125.0,
+                height: 100.0,
                 decoration: BoxDecoration(
                   color: const Color(0XFFB3E5FC),
                   // border:
@@ -51,67 +54,50 @@ class LocationPage extends StatelessWidget {
           Container(
               padding: const EdgeInsets.symmetric(horizontal: 5.0),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                IconButton(
-                  icon: Icon(Icons.add_location),
-                  color: Colors.orange,
-                  onPressed: () {},
-                ),
-                Text(
-                  'Bishal Adhikari',
-                  style: TextStyle(color: Colors.black, fontSize: 17),
-                ),
-              
-                 FlatButton(
-              onPressed: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (context) => AddressPage()));
-              },
-                padding: const EdgeInsets.fromLTRB(180, 0, 30, 0),
-              child: Text(
-                "Edit",
-                style: TextStyle( decoration: TextDecoration.underline,color: Colors.blue, fontSize: 15),
-              ),
-            ),
-          
-              ])),
-         
-          
-           
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.max,
+                  children: <Widget>[
+                    Icon(
+                      Icons.add_location,
+                      color: Colors.orange,
+                    ),
+                    Text(
+                      'Bishal Adhikari',
+                      style: TextStyle(color: Colors.black, fontSize: 17),
+                    ),
+                    Spacer(),
+                    FlatButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => AddressPage()));
+                      },
+                      // padding: const EdgeInsets.fromLTRB(180, 0, 30, 0),
+                      child: Text(
+                        "Edit",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue,
+                            fontSize: 15),
+                      ),
+                    ),
+                  ])),
           Container(
-            padding: const EdgeInsets.fromLTRB(55, 0, 30, 0),
-            child: Text(
-              '9812345678',
-              style: TextStyle(color: Colors.black, fontSize: 17),
-            ),
-          ),
-          Row(
-           
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.fromLTRB(55, 0, 30, 0),
-                child: Row(children: <Widget>[
-                  Text(
-                    'Bagmati,Kathmandu,New Baneshwor',
-                    style: TextStyle(color: Colors.black, fontSize: 15),
-                  )
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('9806050253',
+                      style: TextStyle(color: Colors.black, fontSize: 17)),
+                  Text('Bagmati,Kathmandu,New Baneshwor',
+                      style: TextStyle(color: Colors.black, fontSize: 17)),
+                  Text('Area, Buddhanagar',
+                      style: TextStyle(color: Colors.black, fontSize: 17)),
                 ]),
-              )
-            ],
-          ),
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 55.0),
-                child: Row(children: <Widget>[
-                  Text(
-                    'Area, Buddhanagar',
-                    style: TextStyle(color: Colors.black, fontSize: 15),
-                  )
-                ]),
-              )
-            ],
           ),
         ]));
   }
