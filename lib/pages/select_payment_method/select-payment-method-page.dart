97fb5d00196b594ad69c1ae7de9e6e67a0958145
Bishal <actionbishal98130@gmail.com>
2020-10-18 01:45:48 +0545

    updated

--- lib/pages/select_payment_method/select-payment-method-page.dart
+++ lib/pages/select_payment_method/select-payment-method-page.dart
@@ -12,59 +12,60 @@ class _SelectPaymentMethodPageState extends State<SelectPaymentMethodPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black.withOpacity(0.01),
       appBar: SelectPaymentMethodAppBar(),
       body: SelectPaymentBody(),
-//      bottomNavigationBar: Container(
-//        color: Colors.white,
-//        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-//        height: 80,
-//        child: Column(
-//          children: [
-//            Row(
-//              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//              children: [
-//                Text(
-//                  "Subtotal",
-//                  style: TextStyle(
-////                      fontWeight: FontWeight.bold,
-//                      color: Colors.black,
-//                      fontSize: 16),
-//                ),
-//                Text(
-//                  "Rs 4,201",
-//                  style: TextStyle(
-//                      fontWeight: FontWeight.w400,
-//                      color: Colors.black,
-//                      fontSize: 16),
-//                )
-//              ],
-//            ),
-//            SizedBox(
-//              height:5
-//            ),
-//            Row(
-//              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//
-//              children: [
-//                Text(
-//                  "Total Amount",
-//                  style: TextStyle(
+      bottomNavigationBar: Container(
+        color: Colors.white,
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        height: 80,
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Subtotal",
+                  style: TextStyle(
 //                      fontWeight: FontWeight.bold,
-//                      color: Colors.black,
-//                      fontSize: 16),
-//                ),
-//                Text(
-//                  "Rs 4,201",
-//                  style: TextStyle(
-//                      fontWeight: FontWeight.bold,
-//                      color: NPrimaryColor,
-//                      fontSize: 16),
-//                )
-//              ],
-//            )
-//          ],
-//        ),
-//      ),
+                      color: Colors.black,
+                      fontSize: 16),
+                ),
+                Text(
+                  "Rs 4,201",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w400,
+                      color: Colors.black,
+                      fontSize: 16),
+                )
+              ],
+            ),
+            SizedBox(
+              height:5
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+              children: [
+                Text(
+                  "Total Amount",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 16),
+                ),
+                Text(
+                  "Rs 4,201",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: NPrimaryColor,
+                      fontSize: 16),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
