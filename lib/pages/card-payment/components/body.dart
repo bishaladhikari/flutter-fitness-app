ccdc4066bc52064eecf1135669d7ea59864249e4
Bishal <actionbishal98130@gmail.com>
2020-10-17 15:51:03 +0545

    updated

--- lib/pages/card-payment/components/body.dart
+++ lib/pages/card-payment/components/body.dart
@@ -1,5 +1,6 @@
 import 'package:ecapp/constants.dart';
 import 'package:flutter/material.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class Body extends StatefulWidget {
   @override
@@ -7,6 +8,12 @@ class Body extends StatefulWidget {
 }
 
 class _BodyState extends State<Body> {
+  final _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -32,12 +39,14 @@ class _BodyState extends State<Body> {
 //                    size: 18,
                     color: NPrimaryColor,
                   ),
-                  SizedBox(width: 4.0,),
+                  SizedBox(
+                    width: 4.0,
+                  ),
                   Container(
                       width: 80,
                       child: Text(
                         "Security Guaranteed",
-                        style: TextStyle(color: NPrimaryColor,fontSize: 12),
+                        style: TextStyle(color: NPrimaryColor, fontSize: 12),
                       ))
                 ],
               ),
@@ -55,8 +64,120 @@ class _BodyState extends State<Body> {
                       )
                     ],
                   ),
-                  SizedBox(height: 8.0,),
-                  Text("Form Goes here")
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Form(
+                    key: _formKey,
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          controller: emailController,
+                          style: TextStyle(color: Color(0xFF000000)),
+                          cursorColor: Color(0xFF9b9b9b),
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.mail_outline),
+                              border: OutlineInputBorder(),
+                              contentPadding: new EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 10.0),
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Card Number"),
+                          validator: MultiValidator([
+                            RequiredValidator(errorText: "Card Number is required"),
+                            EmailValidator(errorText: "Not A Valid Email"),
+                          ]),
+                        ),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          controller: passwordController,
+                          style: TextStyle(color: Color(0xFF000000)),
+                          cursorColor: Color(0xFF9b9b9b),
+                          keyboardType: TextInputType.visiblePassword,
+                          decoration: InputDecoration(
+                              contentPadding: new EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 10.0),
+                              border: OutlineInputBorder(),
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Card Name"),
+                          validator: MultiValidator([
+                            RequiredValidator(
+                                errorText: "Card Name is Required"),
+                          ]),
+                        ),
+                        SizedBox(height: 10),
+                        TextFormField(
+                          controller: emailController,
+                          style: TextStyle(color: Color(0xFF000000)),
+                          cursorColor: Color(0xFF9b9b9b),
+                          keyboardType: TextInputType.text,
+                          decoration: InputDecoration(
+                              prefixIcon: const Icon(Icons.mail_outline),
+                              border: OutlineInputBorder(),
+                              contentPadding: new EdgeInsets.symmetric(
+                                  vertical: 10.0, horizontal: 10.0),
+                              hintStyle: TextStyle(color: Colors.grey),
+                              hintText: "Card Number"),
+                          validator: MultiValidator([
+                            RequiredValidator(errorText: "Card Number is required"),
+                            EmailValidator(errorText: "Not A Valid Email"),
+                          ]),
+                        ),
+                        SizedBox(height: 10),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+              height: 80,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        "Subtotal",
+                        style: TextStyle(
+//                      fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16),
+                      ),
+                      Text(
+                        "Rs 4,201",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                            fontSize: 16),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                      height:5
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                    children: [
+                      Text(
+                        "Total Amount",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black,
+                            fontSize: 16),
+                      ),
+                      Text(
+                        "Rs 4,201",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: NPrimaryColor,
+                            fontSize: 16),
+                      )
+                    ],
+                  )
                 ],
               ),
             )
--- lib/pages/select_payment_method/select-payment-method-page.dart
+++ lib/pages/select_payment_method/select-payment-method-page.dart
@@ -14,57 +14,57 @@ class _SelectPaymentMethodPageState extends State<SelectPaymentMethodPage> {
     return Scaffold(
       appBar: SelectPaymentMethodAppBar(),
       body: SelectPaymentBody(),
-      bottomNavigationBar: Container(
-        color: Colors.white,
-        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-        height: 80,
-        child: Column(
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Subtotal",
-                  style: TextStyle(
+//      bottomNavigationBar: Container(
+//        color: Colors.white,
+//        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+//        height: 80,
+//        child: Column(
+//          children: [
+//            Row(
+//              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//              children: [
+//                Text(
+//                  "Subtotal",
+//                  style: TextStyle(
+////                      fontWeight: FontWeight.bold,
+//                      color: Colors.black,
+//                      fontSize: 16),
+//                ),
+//                Text(
+//                  "Rs 4,201",
+//                  style: TextStyle(
+//                      fontWeight: FontWeight.w400,
+//                      color: Colors.black,
+//                      fontSize: 16),
+//                )
+//              ],
+//            ),
+//            SizedBox(
+//              height:5
+//            ),
+//            Row(
+//              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//
+//              children: [
+//                Text(
+//                  "Total Amount",
+//                  style: TextStyle(
 //                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: 16),
-                ),
-                Text(
-                  "Rs 4,201",
-                  style: TextStyle(
-                      fontWeight: FontWeight.w400,
-                      color: Colors.black,
-                      fontSize: 16),
-                )
-              ],
-            ),
-            SizedBox(
-              height:5
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-
-              children: [
-                Text(
-                  "Total Amount",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: 16),
-                ),
-                Text(
-                  "Rs 4,201",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: NPrimaryColor,
-                      fontSize: 16),
-                )
-              ],
-            )
-          ],
-        ),
-      ),
+//                      color: Colors.black,
+//                      fontSize: 16),
+//                ),
+//                Text(
+//                  "Rs 4,201",
+//                  style: TextStyle(
+//                      fontWeight: FontWeight.bold,
+//                      color: NPrimaryColor,
+//                      fontSize: 16),
+//                )
+//              ],
+//            )
+//          ],
+//        ),
+//      ),
     );
   }
 }
