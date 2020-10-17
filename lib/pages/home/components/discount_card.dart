6eca543eab179535f5814a285b4d054d5910f7b3
Bishal <actionbishal98130@gmail.com>
2020-10-17 18:50:27 +0545

    updated

--- lib/pages/home/components/discount_card.dart
+++ lib/pages/home/components/discount_card.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:ecapp/bloc/banner_bloc.dart';
 import 'package:flutter/material.dart';
@@ -13,110 +14,88 @@ class DiscountCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     bannerBloc.getBanners();
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          StreamBuilder(
-            stream: bannerBloc.banners,
-            builder: (context, snapShot) {
-              if (snapShot.hasData) {
-                return CarouselSlider.builder(
-                    itemCount: snapShot.data.data.length,
-                    options: CarouselOptions(
-                        height: 166.0,
-                        autoPlay: true,
-                        autoPlayInterval: Duration(seconds: 3),
-                        autoPlayAnimationDuration: Duration(milliseconds: 800),
-                        autoPlayCurve: Curves.fastOutSlowIn),
-                    itemBuilder: (BuildContext context, int itemIndex) {
-                      final item = snapShot.data.data[itemIndex];
-                      return Container(
-                        margin: EdgeInsets.symmetric(vertical: 20),
-                        width: double.infinity,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            image: DecorationImage(
-                                image: NetworkImage(
-                              item.imageThumbnail,
-                            ))),
-                        child: DecoratedBox(
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            gradient: LinearGradient(
-                              colors: [
-                                Color(0xFFFF961F).withOpacity(0.7),
-                                kPrimaryColor.withOpacity(0.7),
-                              ],
-                            ),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(20.0),
-                            child: Row(
-                              children: <Widget>[
-                                Expanded(
-                                  child: SvgPicture.asset(
-                                      "assets/icons/macdonalds.svg"),
-                                ),
-                                Expanded(
-                                  child: RichText(
-                                    text: TextSpan(
-                                      style: TextStyle(color: Colors.white),
-                                      children: [
-                                        TextSpan(
-                                          text: item.caption.toString(),
-                                          style: TextStyle(fontSize: 16),
-                                        ),
-                                        // TextSpan(
-                                        //   text: "30% \n",
-                                        //   style: TextStyle(
-                                        //     fontSize: 43,
-                                        //     fontWeight: FontWeight.bold,
-                                        //   ),
-                                        // ),
-                                        TextSpan(
-                                          text:
-                                              "at MacDonald's on your first order & Instant cashback",
-                                          style: TextStyle(fontSize: 10),
-                                        ),
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              ],
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        StreamBuilder(
+          stream: bannerBloc.banners,
+          builder: (context, snapShot) {
+            if (snapShot.hasData) {
+              return CarouselSlider.builder(
+                  itemCount: snapShot.data.data.length,
+                  options: CarouselOptions(
+                      height: 160.0,
+                      autoPlay: true,
+                      autoPlayInterval: Duration(seconds: 5),
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                      autoPlayCurve: Curves.fastOutSlowIn),
+                  itemBuilder: (BuildContext context, int itemIndex) {
+                    final item = snapShot.data.data[itemIndex];
+                    return Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: const EdgeInsets.all(5.0),
+                      width: double.infinity,
+                      child: CachedNetworkImage(
+                        placeholder: (context, url) => Center(
+                          child: Container(
+                            height: 100,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage("assets/images/placeholder.png"),
+                                  fit: BoxFit.cover),
                             ),
                           ),
                         ),
+                        imageUrl: item.imageThumbnail,
+//            imageUrl: product.imageThumbnail,
+                        imageBuilder: (context, imageProvider) => Container(
+//              width: 75,
+                          height: 100,
+                          decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: imageProvider,
+                                fit: BoxFit.cover,
+                              )),
+                        ),
+                        errorWidget: (context, url, error) => Center(
+                          child: Container(
+                            height: 100,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                  image: AssetImage("assets/images/placeholder.png"),
+                                  fit: BoxFit.cover),
+                            ),
+                          ),
+                        ),
+                      ),
 
-                        // child: Image.network(
-                        //   'https://i.ibb.co/mbWtWMs/beyond-meat-mcdonalds.png',
-                        //   loadingBuilder: (BuildContext context, Widget child,
-                        //       ImageChunkEvent loadingProgress) {
-                        //     if (loadingProgress == null) return child;
-                        //     return Center(
-                        //       child: CircularProgressIndicator(
-                        //         value: loadingProgress.expectedTotalBytes != null
-                        //             ? loadingProgress.cumulativeBytesLoaded /
-                        //                 loadingProgress.expectedTotalBytes
-                        //             : null,
-                        //       ),
-                        //     );
-                        //   },
-                        // ),
-                      );
-                    });
+                      // child: Image.network(
+                      //   'https://i.ibb.co/mbWtWMs/beyond-meat-mcdonalds.png',
+                      //   loadingBuilder: (BuildContext context, Widget child,
+                      //       ImageChunkEvent loadingProgress) {
+                      //     if (loadingProgress == null) return child;
+                      //     return Center(
+                      //       child: CircularProgressIndicator(
+                      //         value: loadingProgress.expectedTotalBytes != null
+                      //             ? loadingProgress.cumulativeBytesLoaded /
+                      //                 loadingProgress.expectedTotalBytes
+                      //             : null,
+                      //       ),
+                      //     );
+                      //   },
+                      // ),
+                    );
+                  });
 
-                //       },
-                //     );
-                //   }).toList(),
-                // );
-              }
-              return _buildShimmerWidget();
-            },
-          ),
-        ],
-      ),
+              //       },
+              //     );
+              //   }).toList(),
+              // );
+            }
+            return _buildShimmerWidget();
+          },
+        ),
+      ],
     );
   }
 }
