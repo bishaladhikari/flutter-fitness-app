46602828e726011fc2a24fb156fda04cb91112c6
Prabin Paudel <prabin.paudel60@gmail.com>
2020-10-19 22:56:59 +0545

    update

--- lib/pages/order_review/order_review_page.dart
+++ lib/pages/order_review/order_review_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart';
@@ -35,7 +36,10 @@ class _OrderReviewPageState extends State<OrderReviewPage> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
+              Text("Overall Rating: " + rating.toString(),
+                  style: TextStyle(fontSize: 14, color: Colors.black)),
               SmoothStarRating(
                   allowHalfRating: false,
                   onRated: (v) {
@@ -51,6 +55,9 @@ class _OrderReviewPageState extends State<OrderReviewPage> {
                   borderColor: Colors.orange,
                   spacing: 0.0),
               SizedBox(height: 10),
+              Text("Add a headline ",
+                  style: TextStyle(fontSize: 14, color: Colors.black)),
+              SizedBox(height: 5),
               TextFormField(
                 controller: headingController,
                 style: TextStyle(color: Color(0xFF000000)),
@@ -67,6 +74,9 @@ class _OrderReviewPageState extends State<OrderReviewPage> {
                 ]),
               ),
               SizedBox(height: 10),
+              Text("Write your review ",
+                  style: TextStyle(fontSize: 14, color: Colors.black)),
+              SizedBox(height: 5),
               TextFormField(
                 controller: messageController,
                 style: TextStyle(color: Color(0xFF000000)),
@@ -84,6 +94,16 @@ class _OrderReviewPageState extends State<OrderReviewPage> {
                   RequiredValidator(errorText: "Heading is required"),
                 ]),
               ),
+              RaisedButton(
+                elevation: 5,
+                onPressed: () {},
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0)),
+                child: Text("Submit Review"),
+                color: NPrimaryColor,
+                textColor: Colors.white,
+                splashColor: NPrimaryColor,
+              )
             ],
           ),
         ),
