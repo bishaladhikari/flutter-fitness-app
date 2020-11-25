79bd5fd4b2463711c7d5d3549144a1d30318ea7e
Prabin Paudel <prabin.paudel60@gmail.com>
2020-11-25 21:00:55 +0545

    update

---
+++ lib/models/profile.dart
@@ -0,0 +1,137 @@
+class Profile {
+  int id;
+  int preferredLocale;
+  String firstName;
+  String lastName;
+  String username;
+  String email;
+  String emailVerifiedAt;
+  String mobile;
+  String createdAt;
+  String updatedAt;
+  int customerOrdersCount;
+  int wishlistCount;
+  String fullName;
+  String storeName;
+  String ip;
+  int attributeCount;
+  Customer customer;
+
+  Profile(
+      {this.id,
+      this.preferredLocale,
+      this.firstName,
+      this.lastName,
+      this.username,
+      this.email,
+      this.emailVerifiedAt,
+      this.mobile,
+      this.createdAt,
+      this.updatedAt,
+      this.customerOrdersCount,
+      this.wishlistCount,
+      this.fullName,
+      this.storeName,
+      this.ip,
+      this.attributeCount,
+      this.customer});
+
+  Profile.fromJson(Map<String, dynamic> json) {
+    id = json['id'];
+    preferredLocale = json['preferred_locale'];
+    firstName = json['first_name'];
+    lastName = json['last_name'];
+    username = json['username'];
+    email = json['email'];
+    emailVerifiedAt = json['email_verified_at'];
+    mobile = json['mobile'];
+    createdAt = json['created_at'];
+    updatedAt = json['updated_at'];
+    customerOrdersCount = json['customer_orders_count'];
+    wishlistCount = json['wishlist_count'];
+    fullName = json['full_name'];
+    storeName = json['store_name'];
+    ip = json['ip'];
+    attributeCount = json['attribute_count'];
+    customer = json['customer'] != null
+        ? new Customer.fromJson(json['customer'])
+        : null;
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['id'] = this.id;
+    data['preferred_locale'] = this.preferredLocale;
+    data['first_name'] = this.firstName;
+    data['last_name'] = this.lastName;
+    data['username'] = this.username;
+    data['email'] = this.email;
+    data['email_verified_at'] = this.emailVerifiedAt;
+    data['mobile'] = this.mobile;
+    data['created_at'] = this.createdAt;
+    data['updated_at'] = this.updatedAt;
+    data['customer_orders_count'] = this.customerOrdersCount;
+    data['wishlist_count'] = this.wishlistCount;
+    data['full_name'] = this.fullName;
+    data['store_name'] = this.storeName;
+    data['ip'] = this.ip;
+    data['attribute_count'] = this.attributeCount;
+    if (this.customer != null) {
+      data['customer'] = this.customer.toJson();
+    }
+    return data;
+  }
+}
+
+class Customer {
+  String referCode;
+  int userId;
+  String ip;
+  String actor;
+  String subject;
+  String imageLink;
+  String totalPoints;
+  int totalReferrals;
+  int totalReferralPoints;
+  int totalOrderPoints;
+
+  Customer(
+      {this.referCode,
+      this.userId,
+      this.ip,
+      this.actor,
+      this.subject,
+      this.imageLink,
+      this.totalPoints,
+      this.totalReferrals,
+      this.totalReferralPoints,
+      this.totalOrderPoints});
+
+  Customer.fromJson(Map<String, dynamic> json) {
+    referCode = json['refer_code'];
+    userId = json['user_id'];
+    ip = json['ip'];
+    actor = json['actor'];
+    subject = json['subject'];
+    imageLink = json['image_link'];
+    totalPoints = json['total_points'];
+    totalReferrals = json['total_referrals'];
+    totalReferralPoints = json['total_referral_points'];
+    totalOrderPoints = json['total_order_points'];
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['refer_code'] = this.referCode;
+    data['user_id'] = this.userId;
+    data['ip'] = this.ip;
+    data['actor'] = this.actor;
+    data['subject'] = this.subject;
+    data['image_link'] = this.imageLink;
+    data['total_points'] = this.totalPoints;
+    data['total_referrals'] = this.totalReferrals;
+    data['total_referral_points'] = this.totalReferralPoints;
+    data['total_order_points'] = this.totalOrderPoints;
+    return data;
+  }
+}
--- lib/pages/settings/components/body.dart
+++ lib/pages/settings/components/body.dart
@@ -1,14 +1,29 @@
 import 'package:ecapp/bloc/auth_bloc.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:ecapp/bloc/profile_bloc.dart';
+import 'package:ecapp/constants.dart';
+import 'package:ecapp/models/response/profile_response.dart';
 import 'package:ecapp/pages/review/review-page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:form_field_validator/form_field_validator.dart';
 
 class Body extends StatefulWidget {
   @override
   _BodyState createState() => _BodyState();
 }
 
-class _BodyState extends State<Body> {
+class _BodyState extends State<Body> with SingleTickerProviderStateMixin {
+  final _formKey = GlobalKey<FormState>();
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+
+  TextEditingController firstNameController = TextEditingController();
+  TextEditingController lastNameController = TextEditingController();
+  TextEditingController mobileController = TextEditingController();
+  bool _validate = false;
+  String email;
+
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
@@ -18,7 +33,19 @@ class _BodyState extends State<Body> {
           ListTile(
               title: Text('Account Information'),
               onTap: () {
-                Navigator.pushNamed(context, 'accountInformation', arguments: false);
+                profileBloc.userProfile();
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  builder: (context) => SingleChildScrollView(
+                    child: Container(
+                      padding: EdgeInsets.only(
+                          bottom: MediaQuery.of(context).viewInsets.bottom),
+                      child: profileUpdateForm(),
+                    ),
+                  ),
+                );
+                // Navigator.pushNamed(context, 'accountInformation', arguments: false);
               }),
           ListTile(
               title: Text('Address Book'),
@@ -116,5 +143,179 @@ class _BodyState extends State<Body> {
         });
   }
 
-  initMain() {}
+  profileUpdateForm() {
+    return StreamBuilder<ProfileResponse>(
+        stream: profileBloc.subject.stream,
+        builder: (context, AsyncSnapshot<ProfileResponse> snapshot) {
+          if (snapshot.hasData) {
+            var profile = snapshot.data.profile;
+            firstNameController =
+                TextEditingController(text: profile.firstName);
+            lastNameController = TextEditingController(text: profile.lastName);
+            mobileController = TextEditingController(text: profile.mobile);
+            email = profile.email;
+          }
+
+          return Container(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Form(
+                autovalidate: _validate,
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(tr("Account Information"),
+                            style: TextStyle(
+                                fontSize: 18,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold)),
+                        Spacer(),
+                        IconButton(
+                          alignment: Alignment.centerRight,
+                          icon: Icon(Icons.cancel,
+                              color: Colors.orange, size: 25),
+                          onPressed: () {
+                            Navigator.of(context).pop();
+                          },
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      controller: firstNameController,
+                      style: TextStyle(color: Color(0xFF000000)),
+                      cursorColor: Color(0xFF9b9b9b),
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          contentPadding: new EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 10.0),
+                          hintStyle: TextStyle(color: Colors.grey),
+                          hintText: "First Name"),
+                      validator: MultiValidator([
+                        RequiredValidator(
+                            errorText: "You cannot leave this field empty"),
+                      ]),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      controller: lastNameController,
+                      style: TextStyle(color: Color(0xFF000000)),
+                      cursorColor: Color(0xFF9b9b9b),
+                      keyboardType: TextInputType.text,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          contentPadding: new EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 10.0),
+                          hintStyle: TextStyle(color: Colors.grey),
+                          hintText: "Last Name"),
+                      validator: MultiValidator([
+                        RequiredValidator(errorText: "You cannot leave this field empty"),
+                      ]),
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Container(
+                        alignment: Alignment.topLeft,
+                        child: Text(
+                          tr(email),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Colors.black87),
+                        )),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    TextFormField(
+                      controller: mobileController,
+                      style: TextStyle(color: Color(0xFF000000)),
+                      cursorColor: Color(0xFF9b9b9b),
+                      inputFormatters: <TextInputFormatter>[
+                        WhitelistingTextInputFormatter.digitsOnly,
+                      ],
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          contentPadding: new EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 10.0),
+                          hintStyle: TextStyle(color: Colors.grey),
+                          hintText: "Mobile Number"),
+                      validator: MultiValidator([
+                        RequiredValidator(
+                            errorText: "You cannot leave this field empty"),
+                      ]),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    GestureDetector(
+                      onTap: () => validateUserProfile(context),
+                      child: Container(
+                        height: 50.0,
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                            color: NPrimaryColor,
+                            borderRadius: BorderRadius.circular(5.0)),
+                        child: Center(
+                            child: Text(
+                          "Save",
+                          style: TextStyle(fontSize: 14, color: Colors.white),
+                        )),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+
+  validateUserProfile(context) async {
+    if (_formKey.currentState.validate()) {
+      var params = {
+        "email": "",
+        "first_name": "${firstNameController.text.trim()}",
+        "last_name": "${lastNameController.text.trim()}",
+        "mobile": "${mobileController.text.trim()}",
+      };
+
+      ProfileResponse response = await profileBloc.userProfileUpdate(params);
+
+      if (response.error == null) {
+        Fluttertoast.showToast(
+            msg: "Success",
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.SNACKBAR,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.green,
+            textColor: Colors.white,
+            fontSize: 16.0);
+        Navigator.pop(context);
+      } else {
+        Fluttertoast.showToast(
+            msg: response.error,
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.SNACKBAR,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.red,
+            textColor: Colors.white,
+            fontSize: 16.0);
+      }
+    } else {
+      setState(() => _validate = true);
+    }
+  }
 }
