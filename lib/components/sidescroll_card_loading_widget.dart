e0a2e423b4ce274c9b4e4676494c8301317ef834
Prabin Paudel <prabin.paudel60@gmail.com>
2021-01-19 13:46:51 +0545

    update

--- lib/components/sidescroll_card_loading_widget.dart
+++ lib/components/sidescroll_card_loading_widget.dart
@@ -4,27 +4,24 @@ import 'package:shimmer/shimmer.dart';
 class SideScrollCardLoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: Shimmer.fromColors(
+    return Shimmer.fromColors(
         baseColor: Colors.black26,
         period: Duration(milliseconds: 1000),
         highlightColor: Colors.white70,
-        child: Padding(
-          padding: EdgeInsets.only(top: 10),
-          child: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Container(height: 260, width: 160, color: Colors.black26),
-              SizedBox(width: 5),
-              Container(height: 260, width: 160, color: Colors.black26),
-              SizedBox(width: 5),
-              Container(height: 260, width: 10, color: Colors.black26),
-            ],
-          ),
-        ),
-      ),
-    );
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: List.generate(8, (int index) {
+            return Row(
+              children: [
+                Container(
+                  height: 260,
+                  width: 160,
+                  color: Colors.black26,
+                ),
+                SizedBox(width: 10.0)
+              ],
+            );
+          }),
+        ));
   }
 }
--- lib/pages/home/components/best_sellers_products_list.dart
+++ lib/pages/home/components/best_sellers_products_list.dart
@@ -1,14 +1,11 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:ecapp/bloc/products_bloc.dart';
 import 'package:ecapp/components/product_item.dart';
-import 'package:ecapp/components/product_skeleton.dart';
-import 'package:ecapp/components/search.dart';
 import 'package:ecapp/components/sidescroll_card_loading_widget.dart';
 import 'package:ecapp/constants.dart';
 import 'package:ecapp/models/product.dart';
 import 'package:ecapp/models/response/product_response.dart';
 import 'package:flutter/material.dart';
-import 'package:shimmer/shimmer.dart';
 
 class BestSellersProductsList extends StatefulWidget {
   const BestSellersProductsList({
