bfa1d9f213656e94a6f086d8598a3e187604fd2f
Prabin Paudel <prabin.paudel60@gmail.com>
2021-01-19 21:03:42 +0545

    Update

--- lib/components/sidescroll_card_loading_widget.dart
+++ lib/components/sidescroll_card_loading_widget.dart
@@ -4,24 +4,40 @@ import 'package:shimmer/shimmer.dart';
 class SideScrollCardLoadingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Shimmer.fromColors(
+          child: Row(children: <Widget>[
+            _loadingWidget(),
+            _loadingWidget(),
+            _loadingWidget(),
+            _loadingWidget(),
+            _loadingWidget(),
+            _loadingWidget(),
+            _loadingWidget(),
+            _loadingWidget(),
+          ],),
+          baseColor: Colors.black26,
+          highlightColor: Colors.white70),
+    );
     return Shimmer.fromColors(
         baseColor: Colors.black26,
         period: Duration(milliseconds: 1000),
         highlightColor: Colors.white70,
         child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: List.generate(8, (int index) {
-            return Row(
-              children: [
-                Container(
-                  height: 260,
-                  width: 160,
-                  color: Colors.black26,
-                ),
-                SizedBox(width: 10.0)
-              ],
-            );
-          }),
-        ));
+            scrollDirection: Axis.horizontal,
+            children: <Widget>[_loadingWidget()])
+    );
+  }
+
+  Widget _loadingWidget() {
+    return Row(children: [
+      Container(
+        height: 260,
+        width: 160,
+        color: Colors.black26,
+      ),
+      SizedBox(width: 10.0),
+    ]);
   }
 }
